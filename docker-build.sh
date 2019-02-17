fdc97922c436d449f5d01f6d50581e834299fb83
Ankur Sharma <ankursharma.niit@gmail.com>
2019-02-17 13:14:42 +0530

    init commit

---
+++ docker-build.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+docker build -t thebigdataguru/angular-cli -t thebigdataguru/angular-cli:node.11.9.0-alpine-angular.7.3.1 -f dockerfile-node.11.9.0-alpine-angular.7.3.1
---
+++ docker-push.sh
@@ -0,0 +1,5 @@
+
+#!/usr/bin/env bash
+
+docker push thebigdataguru/angular-cli
+docker push thebigdataguru/angular-cli:node.11.9.0-alpine-angular.7.3.1
