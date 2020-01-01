0b9f55948a44f85b57644cd0043d852fc7305341
Ankur Sharma <ankursharma.niit@gmail.com>
2020-01-01 16:18:11 +0530

    Update docker-push.sh

--- docker-push.sh
+++ docker-push.sh
@@ -4,3 +4,6 @@
 docker push thebigdataguru/angular-cli
 docker push thebigdataguru/angular-cli:node.11.9.0-alpine-angular.7.3.1
 docker push thebigdataguru/angular-cli:node.13.5.0-alpine-angular.8.3.21
+
+docker push thebigdataguru/angular-cli:aws1.16-node.11.9.0-alpine-angular.7.3.1
+docker push thebigdataguru/angular-cli:aws1.16-node.13.5.0-alpine-angular.8.3.21
