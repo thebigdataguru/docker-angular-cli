74a6c8a1babff4b7f01971e4f003203a2bd2278a
Ankur Sharma <ankursharma.niit@gmail.com>
2020-01-01 10:07:31 +0530

    angular v8 image

--- docker-build.sh
+++ docker-build.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
-docker build -t thebigdataguru/angular-cli -t thebigdataguru/angular-cli:node.11.9.0-alpine-angular.7.3.1 -f dockerfile-node.11.9.0-alpine-angular.7.3.1
+docker build -t thebigdataguru/angular-cli -t thebigdataguru/angular-cli:node.11.9.0-alpine-angular.7.3.1 -f node.11.9.0-alpine-angular.7.3.1/dockerfile ./node.11.9.0-alpine-angular.7.3.1
+
+docker build -t thebigdataguru/angular-cli -t thebigdataguru/angular-cli:node.13.5.0-alpine-angular.8.3.21 -f node.13.5.0-alpine-angular.8.3.21/dockerfile ./node.13.5.0-alpine-angular.8.3.21
--- docker-push.sh
+++ docker-push.sh
@@ -3,3 +3,4 @@
 
 docker push thebigdataguru/angular-cli
 docker push thebigdataguru/angular-cli:node.11.9.0-alpine-angular.7.3.1
+docker push thebigdataguru/angular-cli:node.13.5.0-alpine-angular.8.3.21
